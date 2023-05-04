64b123acb28e62946eef5d39f161659dc2dfe4de
saikiranreddyappidi <saikiranreddyvfstr@gmail.com>
2023-05-04 22:00:09 +0530

    Java Script files

---
+++ test.js
@@ -0,0 +1,7 @@
+function testing() {
+  console.log("testing js");
+  document.getElementById("title").innerHTML = "Test finished";
+  document.getElementById("title").style.color = "red";
+  document.getElementById("para").innerHTML = "Hello JavaScript!";
+  alert("Test Complete");
+}
